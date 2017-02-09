6ff6c0afe7dfa937f8f5618f88f71c93ccccac44
Carl Edwin Antonio Nascimento <carlinstr@gmail.com>
2017-02-09 01:22:54 -0200

    Inicio

---
+++ src/main/java/com/carledwin/ti/cobranca/controller/TituloController.java
@@ -0,0 +1,42 @@
+package com.carledwin.ti.cobranca.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.carledwin.ti.cobranca.model.StatusTitulo;
+import com.carledwin.ti.cobranca.model.Titulo;
+import com.carledwin.ti.cobranca.repository.Titulos;
+
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+	
+	@Autowired 
+	private Titulos titulos;
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ModelAndView salvar(Titulo titulo){
+		titulos.save(titulo);
+		ModelAndView mvn = new ModelAndView("CadastroTitulo");
+		mvn.addObject("mensagem","Título salvo com sucesso!");
+		return mvn;
+	}
+
+	@RequestMapping("/novo")
+	public ModelAndView novo(){
+		ModelAndView mvn = new ModelAndView("CadastroTitulo");
+		return mvn;
+	}
+	
+	@ModelAttribute
+	public List<StatusTitulo> statusTitulo(){
+		return Arrays.asList(StatusTitulo.values());
+	}
+}
---
+++ src/main/java/com/carledwin/ti/cobranca/repository/Titulos.java
@@ -0,0 +1,9 @@
+package com.carledwin.ti.cobranca.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.carledwin.ti.cobranca.model.Titulo;
+
+public interface Titulos extends JpaRepository<Titulo, Long> {
+
+}
